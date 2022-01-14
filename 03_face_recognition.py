1b29003c5f7c2f8403ae8b190393d7216000f6bd
ogane <>
2022-01-14 14:05:09 +0900

    変化量を計算するプログラムを03_face_recognition.pyにマージした．

--- 03_face_recognition.py
+++ 03_face_recognition.py
@@ -3,6 +3,7 @@ import time
 import os
 from monitor_ctl import MonitorCtl
 from mtcnn import MTCNN
+from difference_area import calc_amount_of_area
 
 
 def main():
@@ -34,12 +35,33 @@ def main():
     mc = MonitorCtl()
     unknown_count = 0
 
+    # 変化量の計算用
+    avg = None
+    loop_count = 0
+
     while True:
+        # 開始直後はおかしな画像が入るので無視する
+        if loop_count <= 5:
+            loop_count += 1
+            continue
+
         try:
             ret, img = cam.read()
-            gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # BGRからRGBに変換
+            gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)   # グレースケールに変換
+            rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)     # BGRからRGBに変換
             mtcnn_dets = mtcnn_detector.detect_faces(rgb_img)  # MTCNN顔検出
+
+            # 前フレームを保存
+            if avg is None:
+                avg = gray_img.copy().astype("float")
+                continue
+
+            # 変化量を計算
+            change_rate_img, change_rate = calc_amount_of_area(gray_img, avg)
+            print(f'Change_Rate = {change_rate}%')
+
+            # リアルタイムに差分領域を表示
+            cv2.imshow('Change_Rate_img', change_rate_img)
 
             print(unknown_count)
             if unknown_count > 3:
--- difference_area.py
+++ difference_area.py
@@ -1,5 +1,35 @@
 import cv2
 
+
+def calc_amount_of_area(gray_img, avg):
+    # 現在のフレームと移動平均との間の差を計算する
+    # accumulateWeighted関数の第三引数は「どれくらいの早さで以前の画像を忘れるか」。小さければ小さいほど「最新の画像」を重視する。
+    # http://opencv.jp/opencv-2svn/cpp/imgproc_motion_analysis_and_object_tracking.html
+    # 小さくしないと前のフレームの残像が残る
+    # 重みは蓄積し続ける。
+    cv2.accumulateWeighted(gray_img, avg, 0.1)
+    frameDelta = cv2.absdiff(gray_img, cv2.convertScaleAbs(avg))
+
+    # 閾値を設定し、フレームを2値化
+    thresh = cv2.threshold(frameDelta, 80, 255, cv2.THRESH_BINARY)[1]
+
+    # 全体の画素数
+    all_area_pixel = thresh.size
+    # 白部分と黒部分の画素数
+    white_area_pixel = cv2.countNonZero(thresh)
+    black_area_pixel = all_area_pixel - white_area_pixel
+
+    # 白色と黒色の割合
+    white_area = white_area_pixel / all_area_pixel * 100
+    black_area = black_area_pixel / all_area_pixel * 100
+
+    # それぞれの割合を表示
+    # print(f'White_Area = {white_area}%')
+    # print(f'Black_Area = {black_area}%\n')
+
+    return thresh, white_area
+
+
 def main():
     # キャプチャの設定
     cam = cv2.VideoCapture(0)
@@ -7,49 +37,29 @@ def main():
     cam.set(4, 480) # 高さ
 
     avg = None
-    count = 0
+    loop_count = 0
 
     while(True):
         ret, frame = cam.read()
 
         # 開始直後はおかしな画像が入るので無視する
-        if count <= 5:
-            count += 1
+        if loop_count <= 5:
+            loop_count += 1
             continue
 
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # グレースケールに変換
+        gray_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
         # 前フレームを保存
         if avg is None:
-            avg = gray.copy().astype("float")
+            avg = gray_img.copy().astype("float")
             continue
 
-        # 現在のフレームと移動平均との間の差を計算する
-        # accumulateWeighted関数の第三引数は「どれくらいの早さで以前の画像を忘れるか」。小さければ小さいほど「最新の画像」を重視する。
-        # http://opencv.jp/opencv-2svn/cpp/imgproc_motion_analysis_and_object_tracking.html
-        # 小さくしないと前のフレームの残像が残る
-        # 重みは蓄積し続ける。
-        cv2.accumulateWeighted(gray, avg, 0.1)
-        frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
-
-        # 閾値を設定し、フレームを2値化
-        thresh = cv2.threshold(frameDelta, 80, 255, cv2.THRESH_BINARY)[1]
-
+        change_rate_img, change_rate = calc_amount_of_area(gray_img, avg)
+        print(f'Change_Rate = {change_rate}%')
+        
         # リアルタイムに差分領域を表示
-        cv2.imshow('th', thresh)
-
-        # 全体の画素数
-        all_area_pixel = thresh.size
-        # 白部分と黒部分の画素数
-        white_area_pixel = cv2.countNonZero(thresh)
-        black_area_pixel = all_area_pixel - white_area_pixel
-
-        # 白色と黒色の割合
-        white_area = white_area_pixel / all_area_pixel * 100
-        black_area = black_area_pixel / all_area_pixel * 100
-
-        #それぞれの割合を表示
-        print(f'White_Area = {white_area}%')
-        # print(f'Black_Area = {black_area}%\n')
+        cv2.imshow('th', change_rate_img)
 
         # ESCキーで終了
         k = cv2.waitKey(100) & 0xff
